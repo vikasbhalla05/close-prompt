878be9d784e8615e181020a6a2eb3da907b1750d
vikasbhalla05 <77003706+vikasbhalla05@users.noreply.github.com>
2021-09-30 09:48:30 +0530

    final changes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+// selecting the elements to click
 const openModal= document.querySelector('.openModal');
 const closeModal = document.querySelector('.closeModal');
 const greyScr = document.querySelector('.overlay');
 
-openModal.addEventListener('click', function(){
+// opening the modal or message
+openModal.addEventListener( 'click',function(){
 	greyScr.style.visibility = 'visible';
 });
+// closing the modal or message
 closeModal.addEventListener('click', function(){
 	greyScr.style.visibility = 'hidden';
 });
